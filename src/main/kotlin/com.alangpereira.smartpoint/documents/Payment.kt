4e67b457ad3ed3efd94e73124c5693c5a0507ac6
Alan Pereira <alan.pereira@vizir.com.br>
2021-08-02 14:15:53 -0300

    renamed document

--- src/main/kotlin/com.alangpereira.smartpoint/documents/Payment.kt
+++ src/main/kotlin/com.alangpereira.smartpoint/documents/Payment.kt
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
 @Document
-data class Posting (
+data class Payment (
   val date: Date, 
   val type: TypeEnum,
   val employeeId: String,
